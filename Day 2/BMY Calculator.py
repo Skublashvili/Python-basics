ef787863877dd77193e5b85190fc792bdf2937b4
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-03 00:38:52 +0400

    Update BMY Calculator.py

--- Day 2/BMY Calculator.py
+++ Day 2/BMY Calculator.py
@@ -4,6 +4,6 @@ weight = input("Enter your weight in kg: ")
 new_height = float(height)
 new_weight = float(weight)
 
-bmi = (new_weight / (new_height ** 2))
+bmi = (new_weight / new_height ** 2)
 new_bmi = round(bmi, 2)
 print(new_bmi)
