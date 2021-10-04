a4883a7c12ed9f233f84c401dab230b5842f6f46
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-04 23:47:58 +0400

    Create Pizza order app.py

---
+++ Day 3/Pizza order app.py
@@ -0,0 +1,6 @@
+print("Welcome to the Pizza Deliveries")
+size = input("What size pizza do you want? S, M or L: ")
+add_pepperoni = input("Do you want to add peperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+
