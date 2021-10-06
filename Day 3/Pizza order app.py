b3d56f037c927cbcbdb752cc242d9e31c25e62c7
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-07 00:15:21 +0400

    Update Pizza order app.py

--- Day 3/Pizza order app.py
+++ Day 3/Pizza order app.py
@@ -3,4 +3,37 @@ size = input("What size pizza do you want? S, M or L: ")
 add_pepperoni = input("Do you want to add peperoni? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Y":
+        bill += 2
+        if extra_cheese == "Y":
+            bill += 1
+        else:
+            bill += 0
+    else:
+        bill += 0
+elif size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1
+        else:
+            bill += 0
+    else:
+        bill += 0
+else:
+    bill = 25
+    if add_pepperoni == "Y":
+        bill += 3
+        if extra_cheese == "Y":
+            bill += 1
+        else:
+            bill += 0
+    else:
+        bill += 0
+print(f"Your final bill is {bill}")
+
+
 
