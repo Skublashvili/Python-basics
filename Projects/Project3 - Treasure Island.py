7de70fc35059c8c51e4154620487d11dbaca4dad
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-18 23:59:23 +0400

    Update Project3 - Treasure Island.py

--- Projects/Project3 - Treasure Island.py
+++ Projects/Project3 - Treasure Island.py
@@ -22,3 +22,4 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+road = input("You're at a cross road. Where do you want to go? " + 'Type: "left" or "right" ')
