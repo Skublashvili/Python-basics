ff165a0214cdad285baf8445011c5910fc007348
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-19 02:06:09 +0400

    Update Project3 - Treasure Island.py

--- Projects/Project3 - Treasure Island.py
+++ Projects/Project3 - Treasure Island.py
@@ -22,4 +22,18 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-road = input("You're at a cross road. Where do you want to go? " + 'Type: "left" or "right" ')
+road = input("You're at a cross road. Where do you want to go? " + 'Type: "left" or "right"\n').lower()
+if road == "left":
+    lake = input('You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if lake == "wait":
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
+        if door == "yellow":
+            print("You have found a box full of treasure. Well done!")
+        elif door == "blue":
+            print("You entered a room of beasts.\nGame Over.")
+        else:
+            print("You entered a room of a cursed demon.\nGame Over.")
+    else:
+        print("You tried to swim across the lake full of piranhas.\nGame Over.")
+else:
+    print("Unfortunately this road was controled by bandits.\nGame Over.")
