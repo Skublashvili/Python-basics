7ead6355545eb475c2ba841f52d4cc7cd6c1d2e6
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-29 23:39:58 +0400

    Update Treasure Map.py

--- Day 4/Treasure Map.py
+++ Day 4/Treasure Map.py
@@ -1,5 +1,19 @@
-row1 = ["#","#","#"]
-row2 = ["#","#","#"]
-row3 = ["#","#","#"]
+row1 = ["⬜","⬜","⬜"]
+row2 = ["⬜","⬜","⬜"]
+row3 = ["⬜","⬜","⬜"]
 map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Write numbers of column and row where do you want to put the treaasure: ")
+
+column = int(position[0]) - 1
+row = int(position[1])
+print(column)
+
+if row == 1:
+    row1[column] = "X"
+elif row == 2:
+    row2[column] = "X"
+else:
+    row3[column] = "X"
+
 print(f"{row1}\n{row2}\n{row3}")
