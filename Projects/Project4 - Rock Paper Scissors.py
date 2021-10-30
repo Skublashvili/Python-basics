5a27db01dcb24b759f7d70f95f94b3efb8cace8e
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-31 01:15:39 +0400

    Update Project4 - Rock Paper Scissors.py

--- Projects/Project4 - Rock Paper Scissors.py
+++ Projects/Project4 - Rock Paper Scissors.py
@@ -1,3 +1,39 @@
 import random
 
-choise = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+player_choise = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+if player_choise == 0:
+    print(rock)
+elif player_choise == 1:
+    print(paper)
+else:
+    print(scissors)
+
+
+
+choises = [rock, paper, scissors]
+computer_choise = random.choice(choises)
