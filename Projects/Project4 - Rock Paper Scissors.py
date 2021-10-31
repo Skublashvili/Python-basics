24d5e6029dd11f1ab40ccb510f23f915999e8375
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-10-31 23:32:29 +0400

    Update Project4 - Rock Paper Scissors.py

--- Projects/Project4 - Rock Paper Scissors.py
+++ Projects/Project4 - Rock Paper Scissors.py
@@ -26,14 +26,31 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-if player_choise == 0:
-    print(rock)
-elif player_choise == 1:
-    print(paper)
-else:
-    print(scissors)
-
-
 
 choises = [rock, paper, scissors]
 computer_choise = random.choice(choises)
+
+if player_choise == 0:
+    print("Your choise:\n" + rock)
+    if computer_choise == rock:
+        print("Computer choise:\n" + rock + "\nDraw!")
+    elif computer_choise == paper:
+        print("Computer choise:\n" + paper + "\nYou Lose!")
+    else:
+        print("Computer choise:\n" + scissors + "\nYou Win!")
+elif player_choise == 1:
+    print("Your choise:\n" + paper)
+    if computer_choise == rock:
+        print("Computer choise:\n" + rock + "\nYou Win!")
+    elif computer_choise == paper:
+        print("Computer choise:\n" + paper + "\nDraw!")
+    else:
+        print("Computer choise:\n" + scissors + "\nYou lose")
+else:
+    print("Your choise:\n" + scissors)
+    if computer_choise == rock:
+        print("Computer choise:\n" + rock + "\nYou Lose!")
+    elif computer_choise == paper:
+        print("Computer choise:\n" + paper + "\nYou Win!")
+    else:
+        print("Computer choise:\n" + scissors + "\nDraw!")
