2e99df5bcb4eb9465671e1f1a4c4c4c88f659df9
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-11-08 23:28:36 +0400

    Update Average Height.py

--- Day 5/Average Height.py
+++ Day 5/Average Height.py
@@ -1,10 +1,19 @@
 student_heights = input("Input a list of student heights: ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
-print(student_heights)
+# print(student_heights)
 
 total_height = 0
 
 for height in student_heights:
     total_height += height
-print(total_height)
+# print(total_height)
+
+total_students = 0
+
+for student in student_heights:
+    total_students += 1
+# print(total_students)
+
+average_height = round(total_height / total_students)
+print(f"Average height of students is {average_height}")
