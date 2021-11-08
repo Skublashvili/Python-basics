6d0897fe009bab844bf3d7f9d0a4abeb6cb5114d
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-11-08 23:39:12 +0400

    Update Average Height.py

--- Day 5/Average Height.py
+++ Day 5/Average Height.py
@@ -3,6 +3,9 @@ for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 # print(student_heights)
 
+# total_height = sum(student_heights)
+# total_students = len(student_heights)
+
 total_height = 0
 
 for height in student_heights:
