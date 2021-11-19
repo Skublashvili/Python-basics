0d88e45840c2d71847cc0bd6811de63210d5b795
Skublashvili <91569745+Skublashvili@users.noreply.github.com>
2021-11-20 01:00:39 +0400

    Update Project5 - Password Generator.py

--- Projects/Project5 - Password Generator.py
+++ Projects/Project5 - Password Generator.py
@@ -8,15 +8,20 @@ number_of_letters= int(input("How many letters would you like in your password?\
 number_of_symbols = int(input(f"How many symbols would you like?\n"))
 number_of_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+password_list = []
 for char in range(1, number_of_letters + 1):
-    password += random.choice(letters)
+    password_list += random.choice(letters)
 
 for char in range(1, number_of_numbers + 1):
-    password += random.choice(numbers)
+    password_list += random.choice(numbers)
 
 for char in range(1, number_of_symbols + 1):
-    password += random.choice(symbols)
+    password_list += random.choice(symbols)
 
-list(password)
-print(password)
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Yor password is: {password}")
